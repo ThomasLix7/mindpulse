173fd3fce6a31b1bfbff394881ea3d2482b119c1
Hongtao Li <lihongtao@Mac.lan>
2025-02-23 15:36:32 +0100

    initial setup

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { Box, Heading, Text } from '@chakra-ui/react'
+
+export default function Home() {
+  return (
+    <Box p={8}>
+      <Heading>Welcome to Next.js with Chakra UI</Heading>
+      <Text mt={4}>This is a sample page using Chakra UI components.</Text>
+    </Box>
+  )
+}
