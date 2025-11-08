74dec92f5019700e9f59445ed7984b31d912875d
ThomasLix7 <lihongtaoix7@gmail.com>
2025-11-08 18:15:30 +0100

    vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
+  serverExternalPackages: ["@supabase/supabase-js", "playwright"],
+  images: {
+    domains: ["chrome.browserless.io"],
+  },
 };
 
 export default nextConfig;
